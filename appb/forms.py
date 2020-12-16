01fbb1c029f9be32d06980a91c8827ff5774a70d
Egi satria <egicuco50@gmail.com>
2020-12-16 19:59:36 +0700

    fitur costume user auth-all

--- appb/forms.py
+++ appb/forms.py
@@ -17,3 +17,29 @@ class UserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2', 'email']
+
+
+# class SignupForm(forms.ModelForm):
+#     def __init__(self, *args, **kwargs):
+#         super(SignupForm, self).__init__(*args, **kwargs)
+#         self.fields['nameD'].widget = forms.TextInput(
+#             attrs={'placeholder': 'Enter first name'})
+#         self.fields['nameB'].widget = forms.TextInput(
+#             attrs={'placeholder': 'Enter last name'})
+
+#     nameD = forms.CharField(max_length=100)
+#     nameB = forms.CharField(max_length=100)
+#     profil = forms.ImageField(help_text="Upload profile image ")
+
+#     class Meta:
+#         model = Costumer
+#         fields = ('nameD', 'nameB',  'profil', 'user')
+
+#     def signup(self, request, user):
+#         # Save your user
+#         user.user = request.user
+#         user.nameD = self.cleaned_data['nameD']
+#         user.nameB = self.cleaned_data['nameB']
+#         user.save()
+#         user.userprofile.profil = self.cleaned_data.get('profil')
+#         user.userprofile.save()
--- appb/models.py
+++ appb/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 
@@ -14,10 +17,10 @@ class Costumer(models.Model):
     nickname = models.CharField(max_length=50, default='nameD')
     publish = models.DateTimeField(auto_now=False, auto_now_add=True)
     update = models.DateTimeField(auto_now=True, auto_now_add=False)
-    email = models.EmailField()
-    nomorhp = models.IntegerField()
+    email = models.EmailField(blank=True, null=True)
+    nomorhp = models.IntegerField(blank=True, null=True)
     profil = models.ImageField(upload_to='media/image',
-                               height_field='height_field', width_field='width_field', max_length=None)
+                               height_field='height_field', width_field='width_field', max_length=None, default='media/image/IMG_20191007_233026_695.jpg')
     height_field = models.IntegerField(default=0)
     width_field = models.IntegerField(default=0)
 
@@ -26,3 +29,27 @@ class Costumer(models.Model):
 
     def __str__(self):
         return f'{self.id}.{self.user}'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_user_Costumer(sender, instance, created, **kwargs):
+    if created:
+        print(instance)
+        Costumer.objects.create(user=instance)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def save_user_Costumer(sender, instance, **kwargs):
+    # instance.Costumer.save()
+    pass
+
+    # def Costumer_receiver(sender, instance, created, *args, **kwargs):
+    #     if created:
+    #         Costumer = Costumer.objects.create(user=instance)
+    #     else:
+    #         instance.Costumer.save()
+
+    # post_save.connect(Costumer_receiver, sender=settings.AUTH_USER_MODEL)
